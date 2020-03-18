7f7041b3e024eee1a13c91d94f9090ebe890fb65
vivek <vivek.koyani@gmail.com>
2020-03-18 23:22:39 +0530

    add css and content

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className='Content'>
+        <p>42 is the answer to everything...</p>
+      </div>
     </div>
   );
 }
