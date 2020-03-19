b90ff29b66f36790cf5d7dc3bd76ec86960c54b7
vivek <vivek.koyani@gmail.com>
2020-03-19 23:14:06 +0530

    add hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,77 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './Header.css';
+import { CSSTransition } from "react-transition-group";
+
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
 
 export default function Header() {
+    const [isNavVisible, setNavVisibility] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+  
+    useEffect(() => {
+      const mediaQuery = window.matchMedia("(max-width: 700px)");
+      mediaQuery.addListener(handleMediaQueryChange);
+      handleMediaQueryChange(mediaQuery);
+  
+      return () => {
+        mediaQuery.removeListener(handleMediaQueryChange);
+      };
+    }, []);
+  
+    const handleMediaQueryChange = mediaQuery => {
+      if (mediaQuery.matches) {
+        setIsSmallScreen(true);
+      } else {
+        setIsSmallScreen(false);
+      }
+    };
+  
+    const toggleNav = () => {
+      setNavVisibility(!isNavVisible);
+    };
+  
     return (
         <header className="Header">
             <img src={require("../logo.svg")} className="Logo" alt="logo" />
+            <CSSTransition
+                in={!isSmallScreen || isNavVisible}
+                timeout={350}
+                classNames="NavAnimation"
+                unmountOnExit
+            >
             <nav className="Nav">
                 <a href="/">Home</a>
                 <a href="/">Articles</a>
                 <a href="/">About</a>
                 <button>Logout</button>
             </nav>
+                </CSSTransition>
+        <button onClick={toggleNav} className="Burger">
+        
+      </button>
         </header>
     );
 }
